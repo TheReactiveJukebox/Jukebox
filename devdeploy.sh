e122ba770280fca43625d21f0064f289c19ada6f
dtext <textores.danny@gmail.com>
2017-06-02 13:31:27 +0200

    [Docker Compose] Add Docker Compose configuration and deployment script for ease of use

---
+++ devdeploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+function generate_ssl {
+    mkdir -p ssl
+    openssl req \
+        -x509 \
+        -newkey rsa:4096 \
+        -keyout ssl/ssl.key \
+        -out ssl/ssl.crt \
+        -days 365 \
+        -nodes \
+        -subj "/C=DE/ST=NRW/L=Dortmund/O=Reactive Jukebox/OU=PG 607/CN=*/"
+}
+
+function build {
+    cd frontend
+    npm run build:prod
+    cd ../backend
+    mvn deploy
+    cd ..
+}
+
+function deploy {
+    docker-compose -f docker-compose.yml.dev up --build  
+}
+
+if ! [ -f ./ssl/ssl.crt ] || ! [ -f ./ssl/ssl.key ]; then
+    generate_ssl
+fi
+
+build
+deploy
