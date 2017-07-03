19e43d5cc7c408bebb71e8e9b6b410e30e02ba9d
Andreas Lang <andiwg@arcor.de>
2017-07-03 11:10:45 +0200

    [Proddeploy] adds proddeploy.sh, fixes docker-compose.yml.prod and updates branches to master.

---
+++ proddeploy.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Deployment script to quickly get a jukebox instance running on every machine.
+
+# Dependencies: openssl, maven, docker, docker-compose
+# All of those need to be available in the shell that is running this script.
+
+# Generates a new SSL certificate in ssl subfolder, which will be mounted by nginx
+function generate_ssl {
+    mkdir -p ssl
+    openssl req \
+        -x509 \
+        -newkey rsa:4096 \
+        -keyout ssl/ssl.key \
+        -out ssl/ssl.crt \
+        -days 365 \
+        -nodes \
+        -subj "/C=DE/ST=NRW/L=Dortmund/O=Reactive Jukebox/OU=PG 607/CN=*/"
+}
+
+# Builds the backend from source
+function build_frontend {
+    cd frontend
+
+    # install dependencies if necessary
+    if ! [ -d "./node_modules" ]; then
+        npm install
+    fi
+
+    # run build script
+    npm run build:prod
+    cd ..
+}
+
+# Builds the backend from source
+function build_backend {
+    cd backend
+    mvn compiler:compile
+    mvn war:war
+    cd ..
+}
+
+# Deploys using docker compose. Will build images if necessary.
+function deploy {
+    docker-compose -f docker-compose.yml.prod up --build  
+}
+
+if ! [ -f ./ssl/ssl.crt ] || ! [ -f ./ssl/ssl.key ]; then
+    generate_ssl
+fi
+    build_frontend
+    build_backend
+
+
+deploy
